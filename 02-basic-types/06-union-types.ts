5e363ac05ddfe8c6a9fb247bc7c55cfe200d2223
Uspex918 <uspex964@gmail.com>
2026-06-17 14:27:39 +0400

    vot

---
+++ 02-basic-types/06-union-types.ts
@@ -0,0 +1,11 @@
+let result: number | string;
+
+result = 10;
+result = "Success";
+
+function printResult(value: number|string ) {
+    console.log(value);
+}
+printResult(result);
+printResult(25);
+printResult("I'm RICH");
